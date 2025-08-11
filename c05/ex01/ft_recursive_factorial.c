cc70d6ea0e2640bfe051f9664600b5ec97b120f1
bchiang21 <brchiang21@gmail.com>
2025-08-11 09:34:18 +0000

    push

--- c05/ex01/ft_recursive_factorial.c
+++ c05/ex01/ft_recursive_factorial.c
@@ -1,3 +1,27 @@
 
 // Factorial, but this time recursive
 
+#include <unistd.h>
+#include <stdio.h>
+
+int ft_recursive_factorial(int nb)
+{
+    if (nb == 1 || nb == 0)
+   {
+    return 1;
+   }
+
+   int sub = ft_recursive_factorial(nb -1);
+   int result = nb * sub;
+   printf("%d!= %d\n", nb, result);
+
+   return result;
+}
+
+int main(void)
+{
+    int x = 5;
+    
+    ft_recursive_factorial(x);
+
+}
